7b530a51c10d5ad4169ebdfef666c87274cda74d
gzmask <gzmask@gmail.com>
2014-10-18 22:05:38 -0600

    added line plane intersection

--- src/slicer/slice.clj
+++ src/slicer/slice.clj
@@ -3,8 +3,10 @@
 ;; Mainly to get the intersection between triangles and a sequence of Z planes.
 (ns slicer.slice)
 
-;; ## dot product
 (defn dot*
+  "
+  Dot Product
+  This is a lazy version of dot product just for 3-tuples"
   [[x1 y1 z1 :as p1]
    [x2 y2 z2 :as p2]]
   (+
@@ -12,19 +14,45 @@
    (* y1 y2)
    (* z1 z2)))
 
-;; ## Ray/Segment and Plane intersection
+(defn norm
+  "
+  Norm of a vector
+  "
+  [[x y z :as v]]
+  (Math/sqrt (dot* v v)))
+
+(defn point-plane 
+  "
+  Distance from point to plane
+  Returns the distance.
+  If result is positive, point is at the side of the normal,
+  if the result is negative, point is at the other side of the normal.
+  "
+  [[x0 y0 z0 :as point]
+   [[x2 y2 z2 :as normal] [x1 y1 z1 :as position] :as plane]]
+  (/
+    (dot* normal (map - point position))
+    (norm normal)))
+
 (defn plane-line-inc
   "
+  Ray/Segment and Plane intersection
   line is represented by start-point and end-point
   plane is represented by normal direction vector (from original) and the position point
-  returns the intersection point, or nil when not having any.
+  returns the intersection point if there is one, 
+  returns nil when not having any, 
+  returns the line when the line is on the plane.
+  i.e:
+  (plane-line-inc [[5 1.1 0] [-2 1.1 0]] [[1 0 0] [0 0 0]]) => [0 1 0]
+  (plane-line-inc [[5 1.1 0] [-2 1.1 0]] [[0 1 0] [0 0 0]]) => nil
+  (plane-line-inc [[5 1.1 0] [-2 1.1 0]] [[0 1 0] [0 1.1 0]]) => [[5 1.1 0] [-2 1.1 0]]
   "
   [[[x1 y1 z1 :as start-point]  [x2 y2 z2 :as end-point] :as line]
    [[x3 y3 z3 :as normal] [x4 y4 z4 :as position] :as plane]]
   (let [d (dot* normal (map - end-point start-point))]
-    (if (= 0 d)
-      nil
-      (/ (dot* normal (map - position start-point)) d)
-      )))
-
-;(plane-line-inc [[1 2 3] [2 2 3]] [[1 0 0] [0 0 0]])
+    (cond (and (zero? (point-plane start-point plane)) (zero? (point-plane end-point plane))) line
+          (zero? d) nil 
+          :else (let [r (/ (dot* normal (map - position start-point)) d)] 
+                  (if (and (<= r 1) (>= r 0)) 
+                    (vec (map + start-point (map * [r r r] (map - end-point start-point))))
+                    nil)))))
