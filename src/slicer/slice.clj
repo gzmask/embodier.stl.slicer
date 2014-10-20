dbcf4371972f7d5fa558124e9272495d3cdff2f7
gzmask <gzmask@gmail.com>
2014-10-20 15:02:45 -0600

    added tests

--- src/slicer/slice.clj
+++ src/slicer/slice.clj
@@ -10,6 +10,8 @@
   This is a lazy version of dot product just for 3-tuples"
   [[x1 y1 z1 :as p1]
    [x2 y2 z2 :as p2]]
+  {:pre [(float? x1) (float? y1) (float? z1)
+         (float? x2) (float? y2) (float? z2)]}
   (+
    (* x1 x2)
    (* y1 y2)
@@ -22,22 +24,19 @@
   [[x y z :as v]]
   (Math/sqrt (dot* v v)))
 
-(defn point-plane 
+(defn point-plane
   "
   Distance from point to plane
 
   Returns the distance.
   If result is positive, point is at the side of the normal,
   if the result is negative, point is at the other side of the normal.
-
-  i.e
-
-    (point-plane [0 -1 0] [[0 1 0] [0 0 0]]) => -1
-    (point-plane [0 1 0] [[0 1 0] [0 0 0]]) => 1
-    (point-plane [0 0 0] [[0 1 0] [0 0 0]]) => 0
   "
   [[x0 y0 z0 :as point]
    [[x2 y2 z2 :as normal] [x1 y1 z1 :as position] :as plane]]
+  {:pre [(float? x1) (float? y1) (float? z1)
+         (float? x2) (float? y2) (float? z2)
+         (float? x0) (float? y0) (float? z0)]}
   (/
     (dot* normal (map - point position))
     (norm normal)))
@@ -49,25 +48,21 @@
 
   line is represented by start-point and end-point
   plane is represented by normal direction vector (from original) and the position point
-  returns the intersection point if there is one, 
-  returns nil when not having any, 
+  returns the intersection point if there is one,
+  returns nil when not having any,
   returns the line when the line is on the plane.
-
-  i.e
-
-    (plane-line-inc [[5 1.1 0] [-2 1.1 0]] [[1 0 0] [0 0 0]]) => [0 1 0]
-    (plane-line-inc [[5 1.1 0] [-2 1.1 0]] [[0 1 0] [0 0 0]]) => nil
-    (plane-line-inc [[0 1 0] [0 2 0]] [[0 1 0] [0 0 0]]) => nil
-    (plane-line-inc [[0 0.1 0] [0 2 0]] [[0 1 0] [0 0 0]]) => nil
-    (plane-line-inc [[5 1.1 0] [-2 1.1 0]] [[0 1 0] [0 1.1 0]]) => [[5 1.1 0] [-2 1.1 0]]
   "
   [[[x1 y1 z1 :as start-point]  [x2 y2 z2 :as end-point] :as line]
    [[x3 y3 z3 :as normal] [x4 y4 z4 :as position] :as plane]]
+  {:pre [(float? x1) (float? y1) (float? z1)
+         (float? x2) (float? y2) (float? z2)
+         (float? x3) (float? y3) (float? z3)
+         (float? x4) (float? y4) (float? z4)]}
   (let [d (dot* normal (map - end-point start-point))]
     (cond (and (zero? (point-plane start-point plane)) (zero? (point-plane end-point plane))) line
-          (zero? d) nil 
-          :else (let [r (/ (dot* normal (map - position start-point)) d)] 
-                  (if (and (<= r 1) (>= r 0)) 
+          (zero? d) nil
+          :else (let [r (/ (dot* normal (map - position start-point)) d)]
+                  (if (and (<= r 1) (>= r 0))
                     (vec (map + start-point (map * [r r r] (map - end-point start-point))))
                     nil)))))
 
@@ -76,35 +71,39 @@
 (defn triangle-plane-inc
   "
   Triangle and Plane intersection
-
-    (triangle-plane-inc [[0.0 1 0] [-1 -1 0] [1 -1 0]] [[0 1 0] [0 0 0]]) => ([-0.5 0 0] [0.5 0 0])
-    (triangle-plane-inc [[0 0 0] [-1 -1 0] [1 -1 0]] [[0 1 0] [0 0 0]]) => [0 0 0]
-    (triangle-plane-inc [[0 -1 0] [-1 -2 0] [1 -2 0]] [[0 1 0] [0 0 0]]) => nil 
+  If the entire triangle sits on the plane, returns the whole triangle;
+  If one segment of the triangle sits on the plane, that's what you got;
+  If a point of the triangle sits there, you have it;
+  Otherwise, you have the intersection
   "
   [[[x1 y1 z1 :as p1]
     [x2 y2 z2 :as p2]
     [x3 y3 z3 :as p3] :as triangle]
-   [[x4 y4 z4 :as normal] 
+   [[x4 y4 z4 :as normal]
     [x5 y5 z5 :as position] :as plane]]
+  {:pre [(float? x1) (float? y1) (float? z1)
+         (float? x2) (float? y2) (float? z2)
+         (float? x3) (float? y3) (float? z3)
+         (float? x5) (float? y5) (float? z5)
+         (float? x4) (float? y4) (float? z4)]}
   (let [ds (map #(point-plane % plane) triangle)]
-    (cond 
-      (= 3 (count (filter zero? ds))) 
+    (cond
+      (= 3 (count (filter zero? ds)))
       triangle
-      (= 2 (count (filter zero? ds))) 
+      (= 2 (count (filter zero? ds)))
       (filter #(zero? (point-plane % plane)) triangle)
-      (= 1 (count (filter zero? ds))) 
+      (= 1 (count (filter zero? ds)))
       (if (neg? (apply * (filter (complement zero?) ds)))
         [(first (filter #(zero? (point-plane % plane)) triangle))
-         (plane-line-inc 
-           (vec (filter #(not (zero? (point-plane % plane))) triangle)) 
-           plane)] 
+         (plane-line-inc
+           (vec (filter #(not (zero? (point-plane % plane))) triangle))
+           plane)]
         (first (filter #(zero? (point-plane % plane)) triangle)))
-      :else 
+      :else
       (if (or (empty? (filter neg? ds)) (empty? (filter pos? ds)))
         nil
         (filter (complement nil?)
-                [(plane-line-inc [p1 p2] plane) 
-                 (plane-line-inc [p2 p3] plane) 
+                [(plane-line-inc [p1 p2] plane)
+                 (plane-line-inc [p2 p3] plane)
                  (plane-line-inc [p3 p1] plane)])
         ))))
-
--- test/embodier_stl_slicer/core_test.clj
+++ test/embodier_stl_slicer/core_test.clj
@@ -1,7 +1,24 @@
 (ns embodier-stl-slicer.core-test
   (:require [clojure.test :refer :all]
-            [embodier-stl-slicer.core :refer :all]))
+            [slicer.slice :refer :all]
+            [slicer.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-point-plane
+  (testing "the point-plane distance."
+    (is (= (point-plane [0.0 -1.0 0.0] [[0.0 1.0 0.0] [0.0 0.0 0.0]]) -1.0))
+    (is (= (point-plane [0.0 1.0 0.0] [[0.0 1.0 0.0] [0.0 0.0 0.0]]) 1.0))
+    (is (= (point-plane [0.0 0.0 0.0] [[0.0 1.0 0.0] [0.0 0.0 0.0]]) 0.0))))
+
+(deftest test-plane-line-inc
+  (testing "plane and line intersection."
+    (is (= [0.0 1.1 0.0] (plane-line-inc [[5.0 1.1 0.0] [-2.0 1.1 0.0]] [[1.0 0.0 0.0] [0.0 0.0 0.0]])))
+    (is (= nil (plane-line-inc [[5.0 1.1 0.0] [-2.0 1.1 0.0]] [[0.0 1.0 0.0] [0.0 0.0 0.0]])))
+    (is (= nil (plane-line-inc [[0.0 1.0 0.0] [0.0 2.0 0.0]] [[0.0 1.0 0.0] [0.0 0.0 0.0]])))
+    (is (= nil (plane-line-inc [[0.0 0.1 0.0] [0.0 2.0 0.0]] [[0.0 1.0 0.0] [0.0 0.0 0.0]])))
+    (is (= [[5.0 1.1 0.0] [-2.0 1.1 0.0]] (plane-line-inc [[5.0 1.1 0.0] [-2.0 1.1 0.0]] [[0.0 1.0 0.0] [0.0 1.1 0.0]])))))
+
+(deftest test-triangle-plane-inc
+  (testing "triangle plane intersection."
+    (is (= '([-0.5 0.0 0.0] [0.5 0.0 0.0]) (triangle-plane-inc [[0.0 1.0 0.0] [-1.0 -1.0 0.0] [1.0 -1.0 0.0]] [[0.0 1.0 0.0] [0.0 0.0 0.0]])))
+    (is (= [0.0 0.0 0.0] (triangle-plane-inc [[0.0 0.0 0.0] [-1.0 -1.0 0.0] [1.0 -1.0 0.0]] [[0.0 1.0 0.0] [0.0 0.0 0.0]])))
+    (is (= nil (triangle-plane-inc [[0.0 -1.0 0.0] [-1.0 -2.0 0.0] [1.0 -2.0 0.0]] [[0.0 1.0 0.0] [0.0 0.0 0.0]])))))
