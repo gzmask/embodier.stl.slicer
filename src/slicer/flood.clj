375afb1e529856f209352c70257f937bd023e8c5
gzmask <gzmask@gmail.com>
2015-02-07 17:01:18 -0600

    starting the flood!

---
+++ src/slicer/flood.clj
@@ -0,0 +1,8 @@
+;after sliced the model, we need to check for watertight and discretized the slice shape
+(ns slicer.flood)
+
+
+(defn line-box-inc
+  "check if a line start and end by two points is intersected with a box"
+  []
+)
